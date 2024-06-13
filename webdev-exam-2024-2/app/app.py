87b26ae63e6b6e4ba022151521a8201f73256716
Ivan <vakin@proton.me>
2024-06-13 17:54:56 +0300

    login page

--- webdev-exam-2024-2/app/app.py
+++ webdev-exam-2024-2/app/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config.from_pyfile('config.py')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form.get('login')
+        password = request.form.get('password')
+        remember = request.form.get('remember')
+        print(login, password, remember, request.form)
+    return render_template('login.html')
+
+# @app.route('/add')
+# def index():
+#     return render_template('add.html')
 
 # python3 -m venv ve
 # . ve/bin/activate -- Linux
